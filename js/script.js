dbfe8cf744ace809b38ef04e87f547992d15329a
Karl George <kelsyerg@gmail.com>
2017-01-25 21:31:49 +0000

    Working on styles

--- js/script.js
+++ js/script.js
@@ -1,20 +1,31 @@
-$(document).ready(function (){
-    
-    $( "#getQuote" ).on("click", function(){
-        
-        var json = $.ajax({
+$(document).ready(function () {
+
+    getQuote();
+
+    $(' #card ').click(function () {
+        $('#card').toggleClass('clicked');
+       // console.log("Clicked");
+        getQuote();
+        //$('#card').removeClass('clicked');
+    });
+
+    function getQuote() {
+            var json = $.ajax({
                 url: 'https://andruxnet-random-famous-quotes.p.mashape.com/', // URL to the random quote API
-                type: 'POST', 
-                data: {}, 
+                type: 'GET',
+                data: {},
                 dataType: 'json',
-                success: function(data) {  
-                    console.log(data);                   
-                    $( ' #quote' ).html("<p>" + data.quote + "</p>" + "<p>Author: " + data.author + "</p>"); 
+                success: function (data) {
+                    console.log(data);
+                    $( '#quote' ).html(data.quote);
+                    $( '#source' ).html(data.author);
+                    console.log(data.author);
                 },
-                error: function(err) { alert(err); },
-                beforeSend: function(xhr) {
-                xhr.setRequestHeader("X-Mashape-Authorization", "lQqIjlKOIRmsh8s1ehmTtJxsj1wjp1S0LmEjsncrFJbFA1tEOW"); 
+                error: function (err) { alert(err); },
+                beforeSend: function (xhr) {
+                    xhr.setRequestHeader("X-Mashape-Authorization", "lQqIjlKOIRmsh8s1ehmTtJxsj1wjp1S0LmEjsncrFJbFA1tEOW");
                 }
             });
-    });
+    }
 });
+
